4e7cfbfec69100c0cecc6b228ebd2f713eec658e
jarturomora <jarturomora@gmail.com>
2026-05-20 20:20:32 +0200

    Añade un caso de prueba

--- test_operaciones.py
+++ test_operaciones.py
@@ -9,6 +9,7 @@ class TestOperaciones(unittest.TestCase):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+        self.assertEqual(sumar(1, 1), 2)
 
     def test_restar(self):
         self.assertEqual(restar(3, 2), 1)
